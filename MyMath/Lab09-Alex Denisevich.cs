9ce464b9e0655ef405186cfde8952bf7c7ad4a52
AlexDenisevich <39071875+ItsJustAlexander@users.noreply.github.com>
2018-12-08 19:26:18 -0500

    Add files via upload

---
+++ MyMath/Lab09-Alex Denisevich.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathLab_Alex_Denisevich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyMath GetResultMultiply = new MyMath();
+            MyMath GetResultDivide = new MyMath();
+            MyMath GetResultSubtract = new MyMath();
+            MyMath GetResultAdd = new MyMath();
+
+            GetResultMultiply.Multiply(5, 5);
+            GetResultDivide.Divide(45, 5);
+            GetResultSubtract.Subtract(21, 9);
+            GetResultAdd.Add(55, 45);
+
+            Console.WriteLine(GetResultMultiply.result);
+            Console.WriteLine(GetResultDivide.result);
+            Console.WriteLine(GetResultSubtract.result);
+            Console.WriteLine(GetResultAdd.result);
+
+        }
+    }
+}
---
+++ MyMath/MyMathLab09-Alex Denisevich.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMathLab_Alex_Denisevich
+{
+    class MyMath
+    {
+        public double result;
+        public double operand1;
+        public double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        {
+            this.result = operand1 * operand2;
+            return;
+        }
+        public void Divide(double operand1, double operand2)
+        {
+            this.result = operand1 / operand2;
+            return;
+        }
+        public void Subtract(double operand1, double operand2)
+        {
+            this.result = operand1 - operand2;
+            return;
+        }
+        public void Add(double operand1, double operand2)
+        {
+            this.result = operand1 + operand2;
+            return;
+        }
+        
+
+        public void GetResult()
+        {
+        }
+
+    }
+}
